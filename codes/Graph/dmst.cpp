4004b58b10ee78a46da347d6aeb6df0a007420c1
hanhanW <hanhan0912@gmail.com>
2014-07-14 13:59:16 +0800

    upload dmst with a solution

---
+++ codes/Graph/dmst.cpp
@@ -0,0 +1,106 @@
+const int INF = 1029384756;
+
+struct edge_t{
+    int u,v,w;
+    set< pair<int,int> > add,sub;
+    edge_t(){
+        u = -1;
+        v = -1;
+        w = 0;
+    }
+    edge_t(int _u, int _v, int _w){
+        u = _u;
+        v = _v;
+        w = _w;
+        add.insert(_MP(_u,_v));
+    }
+    edge_t& operator += (const edge_t& obj) {
+        w += obj.w;
+        FOR (it, obj.add) {
+            if (!sub.count(*it)) add.insert(*it);
+            else sub.erase(*it);
+        }
+        FOR (it, obj.sub) {
+            if (!add.count(*it)) sub.insert(*it);
+            else add.erase(*it);
+        }
+        return *this;
+    }
+
+    edge_t& operator -= (const edge_t& obj) {
+        w -= obj.w;
+        FOR (it, obj.sub) {
+            if (!sub.count(*it)) add.insert(*it);
+            else sub.erase(*it);
+        }
+        FOR (it, obj.add) {
+            if (!add.count(*it)) sub.insert(*it);
+            else add.erase(*it);
+        }
+        return *this;
+    }
+}eg[MXN*MXN],prv[MXN],EDGE_INF(-1,-1,INF);
+int N,M;
+int cycid,incycle[MXN],contracted[MXN];
+vector<int> E[MXN];
+
+edge_t dmst(int rt){
+    edge_t cost;
+    for (int i=0; i<N; i++){
+        contracted[i] = 0;
+        incycle[i] = 0;
+        prv[i] = EDGE_INF;
+    }
+    cycid = 0;
+    int u,v;
+    while (true){
+        for (v=0; v<N; v++){
+            if (v != rt && !contracted[v] && prv[v].w == INF)
+                break;
+        }
+        if (v > N) break; // end
+        for (int i=0; i<M; i++){
+            if (eg[i].v == v && eg[i].w < prv[v].w){
+                prv[v] = eg[i];
+            }
+        }
+        if (prv[v].w == INF){ // not connected
+            return EDGE_INF;
+        }
+        cost += prv[v];
+        for (u=prv[v].u; u!=v && u!=-1; u=prv[u].u);
+        if (u == -1) continue;
+        incycle[v] = ++cycid;
+        for (u=prv[v].u; u!=v; u=prv[u].u){
+            contracted[u] = 1;
+            incycle[u] = cycid;
+        }
+        for (int i=0; i<M; i++){
+            if (incycle[eg[i].u] != cycid && incycle[eg[i].v] == cycid){
+                eg[i] -= prv[eg[i].v];
+            }
+        }
+        for (int i=0; i<M; i++){
+            if (incycle[eg[i].u] == cycid) eg[i].u = v;
+            if (incycle[eg[i].v] == cycid) eg[i].v = v;
+            if (eg[i].u == eg[i].v) eg[i--] = eg[--M];
+        }
+        for (int i=0; i<N; i++){
+            if (contracted[i]) continue;
+            if (prv[i].u>=0 && incycle[prv[i].u] == cycid)
+                prv[i].u = v;
+        }
+        prv[v] = EDGE_INF;
+    }
+    return cost;
+}
+
+void solve(){
+    edge_t cost = dmst(0);
+    FOR(it,cost.add){ // find a solution
+        E[it->_F]._PB(it->_S);
+        prv[it->_S] = edge_t(it->_F,it->_S,0);
+    }
+}
+
+int main(){}
