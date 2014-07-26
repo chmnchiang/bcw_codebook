93020459736a76ea33e060f49d58d05f15b8f941
meteor <bobogei81123@gmail.com>
2014-07-26 14:34:00 +0800

    Delete Evil _

--- codes/Basic/Default_Code/Default_Code.cpp
+++ codes/Basic/Default_Code/Default_Code.cpp
@@ -1,15 +1,15 @@
 #include<bits/stdc++.h>
 using namespace std;
 #define REP(n,i) for(int (i)=0;(i)<(n);(i)++)
-#define CDREP(n,i) for(int (i)=((n)−1);(i)>=0;(i)−−)
-#define CDFOR(s,e,i) for(int i=((e)−1);(i)>=(s);(i)−−)
-#define _SZ(n) memset((n),0,sizeof(n))
-#define _SMO(n) memset((n),−1,sizeof(n))
-#define _MC(n,m) memcpy((n),(m),sizeof(n))
-#define _F first
-#define _S second
-#define _MP(a,b) make_pair((a),(b))
-#define _PB(a) push_back((a))
+#define CDREP(n,i) for(int (i)=((n)-1);(i)>=0;(i)--)
+#define CDFOR(s,e,i) for(int i=((e)-1);(i)>=(s);(i)--)
+#define SZ(n) memset((n),0,sizeof(n))
+#define SMO(n) memset((n),-1,sizeof(n))
+#define MC(n,m) memcpy((n),(m),sizeof(n))
+#define F first
+#define S second
+#define MP(a,b) make_pair((a),(b))
+#define PB(a) push_back((a))
 #define FOR(x,y) for(__typeof(y.begin())x=y.begin();x!=y.end();x++)
 #define IOS ios_base::sync_with_stdio(0);
 // Let's Fight!
