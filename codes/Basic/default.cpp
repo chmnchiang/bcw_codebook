579419d118172e258ff6054375f17532174128e6
hanhanW <hanhan0912@gmail.com>
2014-08-30 23:34:10 +0800

    fix default.cpp

--- codes/Basic/default.cpp
+++ codes/Basic/default.cpp
@@ -1,15 +1,19 @@
 #include<bits/stdc++.h>
+#include<cmath>
+#include<cstdio>
+#include<cstring>
+#include<cstdlib>
+#include<iostream>
+#include<algorithm>
+#include<vector>
 using namespace std;
-#define REP(n,i) for(int (i)=0;(i)<(n);(i)++)
-#define CDREP(n,i) for(int (i)=((n)-1);(i)>=0;(i)--)
-#define CDFOR(s,e,i) for(int i=((e)-1);(i)>=(s);(i)--)
-#define SZ(n) memset((n),0,sizeof(n))
-#define SMO(n) memset((n),-1,sizeof(n))
-#define MC(n,m) memcpy((n),(m),sizeof(n))
+#define _SZ(n) memset((n),0,sizeof(n))
+#define _SMO(n) memset((n),-1,sizeof(n))
+#define _MC(n,m) memcpy((n),(m),sizeof(n))
 #define _F first
 #define _S second
-#define MP(a,b) make_pair((a),(b))
-#define PB(a) push_back((a))
+#define _MP make_pair
+#define _PB push_back
 #define FOR(x,y) for(__typeof(y.begin())x=y.begin();x!=y.end();x++)
 #define IOS ios_base::sync_with_stdio(0)
 // Let's Fight!
