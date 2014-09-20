070d5b796968465f735eb9d9ae23ef906e9d8a50
hanhanW <hanhan0912@gmail.com>
2014-09-20 11:43:28 +0800

    add sais (twt's code)

---
+++ codes/Stringology/Suffix_Array/sais_twt514.cpp
@@ -0,0 +1,50 @@
+struct SA{
+    static const int MXN = 300010;
+    bool _t[MXN*2];
+    int _s[MXN*2], _sa[MXN*2], _c[MXN*2], x[MXN], _p[MXN], _q[MXN*2], hei[MXN], r[MXN];
+    int operator [] (int i){ return _sa[i]; }
+    void build(int *s, int n, int m){
+        memcpy(_s, s, sizeof(int) * n);
+        sais(_s, _sa, _p, _q, _t, _c, n, m);
+        mkhei(n);
+    }
+    void mkhei(int n){
+        REP(i,n) r[_sa[i]] = i;
+        hei[0] = 0;
+        REP(i,n) if(r[i]) {
+            int ans = i>0 ? max(hei[r[i-1]] - 1, 0) : 0;
+            while(_s[i+ans] == _s[_sa[r[i]-1]+ans]) ans++;
+            hei[r[i]] = ans;
+        }
+    }
+    void sais(int *s, int *sa, int *p, int *q, bool *t, int *c, int n, int z){
+        bool uniq = t[n-1] = true, neq;
+        int nn = 0, nmxz = -1, *nsa = sa + n, *ns = s + n, lst = -1;
+#define MS0(x,n) memset((x),0,n*sizeof(*(x)))
+#define MAGIC(XD) MS0(sa, n); \
+        memcpy(x, c, sizeof(int) * z); \
+        XD; \
+        memcpy(x + 1, c, sizeof(int) * (z - 1)); \
+        REP(i,n) if(sa[i] && !t[sa[i]-1]) sa[x[s[sa[i]-1]]++] = sa[i]-1; \
+        memcpy(x, c, sizeof(int) * z); \
+        for(int i = n - 1; i >= 0; i--) if(sa[i] && t[sa[i]-1]) sa[--x[s[sa[i]-1]]] = sa[i]-1;
+        MS0(c, z);
+        REP(i,n) uniq &= ++c[s[i]] < 2;
+        REP(i,z-1) c[i+1] += c[i];
+        if (uniq) { REP(i,n) sa[--c[s[i]]] = i; return; }
+        for(int i = n - 2; i >= 0; i--) t[i] = (s[i]==s[i+1] ? t[i+1] : s[i]<s[i+1]);
+        MAGIC(REP1(i,1,n-1) if(t[i] && !t[i-1]) sa[--x[s[i]]]=p[q[i]=nn++]=i);
+        REP(i, n) if (sa[i] && t[sa[i]] && !t[sa[i]-1]) {
+            neq=lst<0||memcmp(s+sa[i],s+lst,(p[q[sa[i]]+1]-sa[i])*sizeof(int));
+            ns[q[lst=sa[i]]]=nmxz+=neq;
+        }
+        sais(ns, nsa, p + nn, q + n, t + n, c + z, nn, nmxz + 1);
+        MAGIC(for(int i = nn - 1; i >= 0; i--) sa[--x[s[p[nsa[i]]]]] = p[nsa[i]]);
+    }
+};
+
+int main(){
+    // s is int array
+    SA *sa = new SA(); 
+    sa->build(s,n,128);
+}
