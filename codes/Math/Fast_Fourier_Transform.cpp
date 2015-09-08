74a4896085f803a1bc3bea4a6f17eba8fbefbff5
STEP5 <kevin00050@gmail.com>
2015-09-08 18:25:32 +0800

    update FFT

--- codes/Math/Fast_Fourier_Transform.cpp
+++ codes/Math/Fast_Fourier_Transform.cpp
@@ -1,24 +1,29 @@
-typedef complex<double> cplx;
-const double PI = acos(-1);
+typedef long double ld;
+typedef complex<ld> cplx;
+const ld PI = acosl(-1);
 const cplx I(0, 1);
-void fft(int n, cplx a[]) {
-	double theta = 2 * PI / n;
-	for (int m = n; m >= 2; m >>= 1) {
-		int mh = m >> 1;
-		for (int i = 0; i < mh; i++) {
-			cplx w = exp(i*theta*I);
-			for (int j = i; j < n; j += m) {
-				int k = j + mh;
-				cplx x = a[j] - a[k];
-				a[j] += a[k];
-				a[k] = w * x;
-			}
-		}
-		theta *= 2;
-	}
-	int i = 0;
-	for (int j = 1; j < n - 1; j++) {
-		for (int k = n >> 1; k > (i ^= k); k >>= 1);
-		if (j < i) swap(a[i], a[j]);
-	}
+void fft(int n, cplx a[], bool inv)
+{
+  ld theta = 2 * PI / n;
+  for (int m = n; m >= 2; m >>= 1) {
+    int mh = m >> 1;
+    for (int i = 0; i < mh; i++) {
+      cplx w = exp(i * theta * (inv ? -I : I));
+      for (int j = i; j < n; j += m) {
+        int k = j + mh;
+        cplx x = a[j] - a[k];
+        a[j] += a[k];
+        a[k] = w * x;
+      }
+    }
+    theta *= 2;
+  }
+  int i = 0;
+  for (int j = 1; j < n - 1; j++) {
+    for (int k = n >> 1; k > (i ^= k); k >>= 1);
+    if (j < i) swap(a[i], a[j]);
+  }
+  if (inv)
+    for (i = 0; i < n; i++)
+      a[i] /= n;
 }
