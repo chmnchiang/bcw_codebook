d013cb6b81692df1d6c0dfebe976e8e81586d798
hanhanW <hanhan0912@gmail.com>
2015-10-26 09:49:27 +0800

    Add Minimum General Weighted Matching

---
+++ codes/Graph/Matching/Minimum_General_Weighted_Matching.cpp
@@ -0,0 +1,68 @@
+struct Graph {
+  // Minimum General Weighted Matching (Perfect Match)
+  static const int MXN = 105;
+
+  int n, edge[MXN][MXN];
+  int match[MXN],dis[MXN],onstk[MXN];
+  vector<int> stk;
+
+  void init(int _n) {
+    n = _n;
+    FZ(edge);
+  }
+  void add_edge(int u, int v, int w) {
+    edge[u][v] = edge[v][u] = w;
+  }
+  bool SPFA(int u){
+    if (onstk[u]) return true;
+    stk.PB(u);
+    onstk[u] = 1;
+    for (int v=0; v<n; v++){
+      if (u != v && match[u] != v && !onstk[v]){
+        int m = match[v];
+        if (dis[m] > dis[u] - edge[v][m] + edge[u][v]){
+          dis[m] = dis[u] - edge[v][m] + edge[u][v];
+          onstk[v] = 1;
+          stk.PB(v);
+          if (SPFA(m)) return true;
+          stk.pop_back();
+          onstk[v] = 0;
+        }
+      }
+    }
+    onstk[u] = 0;
+    stk.pop_back();
+    return false;
+  }
+
+  int solve() {
+    // find a match
+    for (int i=0; i<n; i+=2){
+      match[i] = i+1;
+      match[i+1] = i;
+    }
+    while (true){
+      int found = 0;
+      FZ(dis); FZ(onstk);
+      for (int i=0; i<n; i++){
+        stk.clear();
+        if (!onstk[i] && SPFA(i)){
+          found = 1;
+          while (SZ(stk)>=2){
+            int u = stk.back(); stk.pop_back();
+            int v = stk.back(); stk.pop_back();
+            match[u] = v;
+            match[v] = u;
+          }
+        }
+      }
+      if (!found) break;
+    }
+    int ret = 0;
+    for (int i=0; i<n; i++)
+      ret += edge[i][match[i]];
+    ret /= 2;
+    return ret;
+  }
+}graph;
+
