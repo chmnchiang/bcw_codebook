415512def5f639636427102b30830a606938d66d
hanhanW <hanhan0912@gmail.com>
2015-11-16 13:21:49 +0800

    fix smallest rotation to bbg version

--- codes/Stringology/smallest_rotation.cpp
+++ codes/Stringology/smallest_rotation.cpp
@@ -1,18 +1,19 @@
-string mcp(string s) {
+string mcp(string s){
   int n = s.length();
-  s = s + s;
+  s += s;
   int i=0, j=1, k=0;
-  while (j<n && k<n) {
+  while (j<n && k<n){
     if (s[i+k] == s[j+k]) k++;
     else {
-      if (s[i+k] < s[j+k]) j += k+1;
-      else i += k+1;
-      k=0;
+      if (s[i+k] < s[j+k]) {
+        j += k + 1;
+      } else {
+        i = j;
+        j = max(j+1, j+k);
+      }
+      k = 0;
     }
-    if (i == j) j++;
   }
-  string ret;
-  for (int l=0; l<n; l++) ret += s[i+l];
-  return ret;
+  return s.substr(i, n);
 }
 
