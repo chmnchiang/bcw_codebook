6d747f61f1ddd5f0710f82e18f9ccee03439ea21
hanhanW <hanhan0912@gmail.com>
2015-11-16 13:08:02 +0800

    fix smallest rotation of string

--- codes/Stringology/smallest_rotation.cpp
+++ codes/Stringology/smallest_rotation.cpp
@@ -1,23 +1,18 @@
-string solve(string str){
-	int N = str.length();
-	str += str;
-	int i=0, j=1, k=0;
-	while (j<N && k<N){
-		if (str[i+k] == str[j+k]) k++;
-		else {
-			if (k == 0 && str[i] > str[j]){
-				i++; j++;
-			} else if (str[i+k] < str[j+k]){
-				j += k+1;
-			} else {
-				i += k+1;
-				j = i + 1;
-			}
-			k=0;
-		}
-	}
-	string ret = "";
-	for (int l=0; l<N; l++)
-		ret += str[i+l];
-	return ret;
+string mcp(string s) {
+  int n = s.length();
+  s = s + s;
+  int i=0, j=1, k=0;
+  while (j<n && k<n) {
+    if (s[i+k] == s[j+k]) k++;
+    else {
+      if (s[i+k] < s[j+k]) j += k+1;
+      else i += k+1;
+      k=0;
+    }
+    if (i == j) j++;
+  }
+  string ret;
+  for (int l=0; l<n; l++) ret += s[i+l];
+  return ret;
 }
+
