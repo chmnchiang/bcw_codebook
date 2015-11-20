b7f099ed3ee71520ce61e65f3ca176523ca4779a
hanhanW <hanhan0912@gmail.com>
2015-11-20 23:27:36 +0800

    add ostream for vector and pair in default.cpp

--- codes/Basic/default.cpp
+++ codes/Basic/default.cpp
@@ -1,16 +1,8 @@
 #include<bits/stdc++.h>
 #include<unistd.h>
-#include<cmath>
-#include<cstdio>
-#include<cstring>
-#include<cstdlib>
-#include<iostream>
-#include<algorithm>
-#include<vector>
 using namespace std;
 #define FZ(n) memset((n),0,sizeof(n))
 #define FMO(n) memset((n),-1,sizeof(n))
-#define MC(n,m) memcpy((n),(m),sizeof(n))
 #define F first
 #define S second
 #define MP make_pair
@@ -18,6 +10,17 @@ using namespace std;
 #define ALL(x) begin(x),end(x)
 #define SZ(x) ((int)(x).size())
 #define IOS ios_base::sync_with_stdio(0); cin.tie(0)
+template<typename A, typename B>
+ostream& operator <<(ostream &s, const pair<A,B> &p) {
+  return s<<"("<<p.first<<","<<p.second<<")";
+}
+template<typename T>
+ostream& operator <<(ostream &s, const vector<T> &c) {
+  s<<"[ ";
+  for (auto it : c) s << it << " ";
+  s<<"]";
+  return s;
+}
 // Let's Fight!
 
 int main() {
