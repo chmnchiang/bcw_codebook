0974ad97baef33a18587b57500204fe1d836f935
hanhanW <hanhan0912@gmail.com>
2015-11-27 09:47:54 +0800

    Add C code of inter circles

---
+++ codes/Geometry/Intersection_of_two_circles/Intersection_of_two_circles.cpp
@@ -0,0 +1,9 @@
+vector<pdd> interCircle(pdd o1, double r1, pdd o2, double r2) {
+  ld d2 = (o1 - o2) * (o1 - o2);
+  ld d = sqrt(d2);
+	if (d > r1+r2) return {};
+  pdd u = 0.5*(o1+o2) + ((r2*r2-r1*r1)/(2*d2))*(o1-o2);
+  double A = sqrt((r1+r2+d) * (r1-r2+d) * (r1+r2-d) * (-r1+r2+d));
+  pdd v = A / (2*d2) * pdd(o1.S-o2.S, -o1.F+o2.F);
+  return {u+v, u-v};
+}
