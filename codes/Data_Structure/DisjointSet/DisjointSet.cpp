00ed9d5fcbaf421a09246611716ed7964a765700
hanhanW <hanhan0912@gmail.com>
2015-12-02 05:26:39 +0800

    Add Disjoint Set

---
+++ codes/Data_Structure/DisjointSet/DisjointSet.cpp
@@ -0,0 +1,42 @@
+struct DisjointSet {
+  // save() is like recursive
+  // undo() is like return
+  int n, fa[MXN], sz[MXN];
+  vector<pair<int*,int>> h;
+  vector<int> sp;
+  void init(int tn) {
+    n=tn;
+    for (int i=0; i<n; i++) {
+      fa[i]=i;
+      sz[i]=1;
+    }
+    sp.clear();
+  }
+  void assign(int *k, int v) {
+    h.PB({k, *k});
+    *k=v;
+  }
+  void save() {
+    sp.PB(SZ(h));
+  }
+  void undo() {
+    assert(!sp.empty());
+    int last=sp.back(); sp.pop_back();
+    while (SZ(h)!=last) {
+      auto x=h.back(); h.pop_back();
+      *x.F=x.S;
+    }
+  }
+  int f(int x) {
+    while (fa[x]!=x) x=fa[x];
+    return x;
+  }
+  void uni(int x, int y) {
+    x=f(x); y=f(y);
+    if (x==y) return ;
+    if (sz[x]<sz[y]) swap(x, y);
+    assign(&sz[x], sz[x]+sz[y]);
+    assign(&fa[y], x);
+  }
+}djs;
+
