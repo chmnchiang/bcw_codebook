aa5871c61817503640bea4f239a64865c3b3d2d5
hanhanW <hanhan0912@gmail.com>
2016-05-11 21:12:31 +0800

    Fix fast linear recurrence (PEC ver.)

--- codes/Math/Fast_Linear_Recurrence.cpp
+++ codes/Math/Fast_Linear_Recurrence.cpp
@@ -1,67 +1,47 @@
-typedef long long ll;
-
-const int MAXN = 514;
-const int LG = 61;
-const ll MOD = 1000000007;
-
-ll N;
-int M;
-ll f[LG][MAXN];
-ll tmp[MAXN*2];
-ll g[MAXN];
-
-void calc_f(ll f0[MAXN], ll f1[MAXN], ll f2[MAXN]) {
-  FZ(tmp);
-  for(int i=0; i<M; i++)
-    for(int j=0; j<M; j++)
-      tmp[i+j] = (tmp[i+j] + f0[i] * f1[j]) % MOD;
-  for(int i=2*M-2; i>=M; i--) {
-    ll mrat = (MOD - tmp[i]) % MOD;
-    tmp[i] = 0;
-    for(int j=0; j<M; j++)
-      tmp[i+j-M+1] = (tmp[i+j-M+1] + mrat * f[0][j]) % MOD;
-    tmp[i-M] = (tmp[i-M] + mrat * (MOD-1)) % MOD;
-  }
-  for(int i=0; i<M; i++)
-    f2[i] = tmp[i];
+ll n,m,dp[N+N];
+void pre_dp(){
+	dp[0]= 1;
+	ll bdr = min(m+m,n);
+	for(ll i=1; i<=bdr; i++)
+		for(ll j=i-1; j>=max(0ll,i-m); j--)
+			dp[i]= add(dp[i],dp[j]);
 }
-void make() {
-  for(int i=1; i<LG; i++)
-    calc_f(f[i-1], f[i-1], f[i]);
-
-  for(int i=0; i<10; i++) {
-    for(int j=0; j<M; j++)
-      cout<<f[i][j]<<" ";
-    cout<<endl;
-  }
+vector<ll> Mul(const vector<ll>& v1,const vector<ll>& v2){
+	int sz1 = (int)v1.size();
+	int sz2 = (int)v2.size();
+	assert(sz1 == m and sz2 == m);
+	vector<ll> _v(m+m);
+	for(int i=0; i<m+m; i++) _v[i]= 0;
+	// expand
+	for(int i=0; i<sz1; i++)
+		for(int j=0; j<sz2; j++)
+			_v[i+j+1]= add(_v[i+j+1],mul(v1[i],v2[j]));
+	// shrink
+	for(int i=0; i<m; i++)
+		for(int j=1; j<=m; j++)
+			_v[i + j]= add(_v[i + j],_v[i]);
+	for(int i=0; i<m; i++)
+		_v[i]= _v[i + m];
+	_v.resize(m);
+	return _v;
 }
-ll calc(ll x) {
-  bool any = false;
-  for(int i=0; i<LG; i++) {
-    if(x&(1LL<<i)) {
-      if(!any) {
-        any = true;
-        for(int j=0; j<M; j++)
-          g[j] = f[i][j];
-      } else {
-        calc_f(g, f[i], g);
-      }
-    }
-  }
-
-  return g[0];
-}
-int main() {
-  cin>>N>>M;
-  for(int i=0; i<M; i++)
-    cin>>f[0][i];
-
-  // f[n] = \sum_{0 <= i < M} f[n-i-1]
-
-  make();
-  ll ans = calc(N);
-
-  cout<<ans<<endl;
-
-  return 0;
+vector<ll> I,A;
+ll solve(){
+	pre_dp();
+	if(n <= m+m)return dp[n];
+	I.resize(m);
+	A.resize(m);
+	for(int i=0; i<m; i++) I[i]=A[i]=1;
+	// dp[n]= /Sum_{i=0}^{m-1} A_i * dp[n - i - 1]
+	ll dlt = (n - m) / m;
+	ll rdlt = dlt * m;
+	while(dlt){
+		if(dlt & 1ll) I = Mul(I,A);
+		A = Mul(A,A);
+		dlt >>= 1;
+	}
+	ll ans = 0;
+	for(int i=0; i<m; i++)
+		ans = add(ans,mul(I[i],dp[n-i-1-rdlt]));
+  return ans;
 }
