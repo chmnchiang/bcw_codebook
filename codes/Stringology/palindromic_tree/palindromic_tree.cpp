7e7cde81fa16f5251e22b3b5d35d864b1f4f379e
hanhanW <hanhan0912@gmail.com>
2016-10-14 18:23:07 +0800

    Fix palindromic tree (long long long long ver.)

--- codes/Stringology/palindromic_tree/palindromic_tree.cpp
+++ codes/Stringology/palindromic_tree/palindromic_tree.cpp
@@ -1,44 +1,54 @@
-int len[maxn];
-int suffLink[maxn];
-int to[maxn][2];
-int cnt[maxn];
-int numV;
-char str[maxn];
-
-int v;
-
-void addLetter(int n)
-{
-    while (str[n - len[v] - 1] != str[n] )
-        v = suffLink[v];
-    int u = suffLink[v];
-    while (str[n - len[u] - 1] != str[n] )
-        u = suffLink[u];
-    int u_ = to[u][str[n] - 'a'];
-    int v_ = to[v][str[n] - 'a'];
-    if (v_ == -1)
-    {
-        v_ = to[v][str[n] - 'a'] = numV;
-        len[numV++] = len[v] + 2;
-        suffLink[v_] = u_;
+struct palindromic_tree{
+  struct node{
+    int next[26],fail,len;
+    int cnt,num,st,ed;
+    node(int l=0):fail(0),len(l),cnt(0),num(0){
+      for(int i=0;i<26;++i)next[i]=0;
     }
-    v = v_;
-    cnt[v]++;
-}
-
-void init()
-{
-    memset(to, -1, sizeof to);
-    str[0] = '#';
-    len[0] = -1;
-    len[1] = 0;
-    len[2] = len[3] = 1;
-    suffLink[1] = 0;
-    suffLink[0] = 0;
-    suffLink[2] = 1;
-    suffLink[3] = 1;
-    to[0][0] = 2;
-    to[0][1] = 3;
-    numV = 4;      
-}
-
+  };
+  vector<node >state;
+  vector<char >s;
+  int last,n;
+  palindromic_tree():state(2),last(1),n(0){
+    state[0].fail=1;
+    state[1].len=-1;
+    s.push_back(-1);
+  }
+  inline void clear(){
+    state.clear();
+    s.clear();
+    last=1;
+    n=0;
+    state.push_back(0);
+    state.push_back(-1);
+    state[0].fail=1;
+    s.push_back(-1);
+  }
+  inline int get_fail(int x){
+    while(s[n-state[x].len-1]!=s[n])x=state[x].fail;
+    return x;
+  }
+  inline void add(int c){
+    s.push_back(c-='a');
+    ++n;
+    int cur=get_fail(last);
+    if(!state[cur].next[c]){
+      int now=state.size();
+      state.push_back(state[cur].len+2);
+      state[now].fail=state[get_fail(state[cur].fail)].next[c];
+      state[cur].next[c]=now;
+      state[now].num=state[state[now].fail].num+1;
+    }
+    last=state[cur].next[c];
+    ++state[last].cnt;
+  }
+  inline void count(){
+    vector<node>::reverse_iterator i=state.rbegin();
+    for(;i!=state.rend();++i){
+      state[i->fail].cnt+=i->cnt;
+    }
+  }
+  inline int size(){
+    return state.size()-2;
+  }
+};
