c82eeae6a37b42dd4e5a9bbe72ba4d8369d8d6af
hanhanW <hanhan0912@gmail.com>
2016-10-23 22:37:09 +0800

    Fixed bug

--- codes/Geometry/Intersection_of_two_circles/Intersection_of_two_circles.cpp
+++ codes/Geometry/Intersection_of_two_circles/Intersection_of_two_circles.cpp
@@ -2,7 +2,8 @@ using ld = double;
 vector<pdd> interCircle(pdd o1, double r1, pdd o2, double r2) {
   ld d2 = (o1 - o2) * (o1 - o2);
   ld d = sqrt(d2);
-	if (d > r1+r2) return {};
+  if (d < abs(r1-r2)) return {};
+  if (d > r1+r2) return {};
   pdd u = 0.5*(o1+o2) + ((r2*r2-r1*r1)/(2*d2))*(o1-o2);
   double A = sqrt((r1+r2+d) * (r1-r2+d) * (r1+r2-d) * (-r1+r2+d));
   pdd v = A / (2*d2) * pdd(o1.S-o2.S, -o1.F+o2.F);
