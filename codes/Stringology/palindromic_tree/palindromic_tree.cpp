ec29ce1a292cefdf388aad1befb5c07f76542d5f
hanhanW <hanhan0912@gmail.com>
2016-10-24 18:36:19 +0800

    Add example of palindromic tree

--- codes/Stringology/palindromic_tree/palindromic_tree.cpp
+++ codes/Stringology/palindromic_tree/palindromic_tree.cpp
@@ -1,3 +1,74 @@
+//bcw0x1bd2 {{{
+#include<bits/stdc++.h>
+#include<unistd.h>
+using namespace std;
+#define F first
+#define S second
+#define MP make_pair
+#define PB push_back
+#define IOS ios_base::sync_with_stdio(0); cin.tie(0);
+#define SZ(x) ((int)((x).size()))
+#define ALL(x) begin(x),end(x)
+#define REP(i,x) for (int i=0; i<(x); i++)
+#define REP1(i,a,b) for (int i=(a); i<=(b); i++)
+
+typedef long long ll;
+typedef pair<int,int> pii;
+typedef pair<ll,ll> pll;
+typedef long double ld;
+
+#ifdef DARKHH
+#define FILEIO(name)
+#else
+#define FILEIO(name) \
+  freopen(name".in", "r", stdin); \
+  freopen(name".out", "w", stdout);
+#endif
+
+#ifdef DARKHH
+template<typename T>
+void _dump( const char* s, T&& head ) { cerr<<s<<"="<<head<<endl; }
+
+template<typename T, typename... Args>
+void _dump( const char* s, T&& head, Args&&... tail ) {
+  int c=0;
+  while ( *s!=',' || c!=0 ) {
+    if ( *s=='(' || *s=='[' || *s=='{' ) c++;
+    if ( *s==')' || *s==']' || *s=='}' ) c--;
+    cerr<<*s++;
+  }
+  cerr<<"="<<head<<", ";
+  _dump(s+1,tail...);
+}
+
+#define dump(...) do { \
+  fprintf(stderr, "%s:%d - ", __PRETTY_FUNCTION__, __LINE__); \
+  _dump(#__VA_ARGS__, __VA_ARGS__); \
+} while (0)
+
+template<typename Iter>
+ostream& _out( ostream &s, Iter b, Iter e ) {
+  s<<"[";
+  for ( auto it=b; it!=e; it++ ) s<<(it==b?"":" ")<<*it;
+  s<<"]";
+  return s;
+}
+
+template<typename A, typename B>
+ostream& operator <<( ostream &s, const pair<A,B> &p ) { return s<<"("<<p.first<<","<<p.second<<")"; }
+template<typename T>
+ostream& operator <<( ostream &s, const vector<T> &c ) { return _out(s,ALL(c)); }
+template<typename T, size_t N>
+ostream& operator <<( ostream &s, const array<T,N> &c ) { return _out(s,ALL(c)); }
+template<typename T>
+ostream& operator <<( ostream &s, const set<T> &c ) { return _out(s,ALL(c)); }
+template<typename A, typename B>
+ostream& operator <<( ostream &s, const map<A,B> &c ) { return _out(s,ALL(c)); }
+#else
+#define dump(...)
+#endif
+// }}}
+
 struct palindromic_tree{
   struct node{
     int next[26],fail,len;
@@ -6,15 +77,11 @@ struct palindromic_tree{
       for(int i=0;i<26;++i)next[i]=0;
     }
   };
-  vector<node >state;
-  vector<char >s;
+  vector<node> state;
+  vector<char> s;
   int last,n;
-  palindromic_tree():state(2),last(1),n(0){
-    state[0].fail=1;
-    state[1].len=-1;
-    s.push_back(-1);
-  }
-  inline void clear(){
+
+  void init(){
     state.clear();
     s.clear();
     last=1;
@@ -24,11 +91,11 @@ struct palindromic_tree{
     state[0].fail=1;
     s.push_back(-1);
   }
-  inline int get_fail(int x){
+  int get_fail(int x){
     while(s[n-state[x].len-1]!=s[n])x=state[x].fail;
     return x;
   }
-  inline void add(int c){
+  void add(int c){
     s.push_back(c-='a');
     ++n;
     int cur=get_fail(last);
@@ -42,13 +109,24 @@ struct palindromic_tree{
     last=state[cur].next[c];
     ++state[last].cnt;
   }
-  inline void count(){
-    vector<node>::reverse_iterator i=state.rbegin();
-    for(;i!=state.rend();++i){
-      state[i->fail].cnt+=i->cnt;
-    }
-  }
-  inline int size(){
+  int size(){
     return state.size()-2;
   }
-};
+}pt;
+
+int main() {
+  string s;
+  cin >> s;
+  pt.init();
+  for (int i=0; i<SZ(s); i++) {
+    int prvsz = pt.size();
+    pt.add(s[i]);
+    if (prvsz != pt.size()) {
+      int r = i;
+      int l = r - pt.state[pt.last].len + 1;
+      cout << "Find pal @ [" << l << " " << r << "] : " << s.substr(l,r-l+1) << endl;
+    }
+  }
+
+  return 0;
+}
