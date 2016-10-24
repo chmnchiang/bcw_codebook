89aea60e87f238e4c6e1e2cd679a4d7b8622cb3a
hanhanW <hanhan0912@gmail.com>
2016-10-24 18:27:45 +0800

    Make included well and add cc_hash_table

--- codes/Data_Structure/Balance_Tree/extc_bt.cpp
+++ codes/Data_Structure/Balance_Tree/extc_bt.cpp
@@ -1,6 +1,11 @@
-#include <bits/extc++.h>
+#include <ext/pb_ds/assoc_container.hpp>
+using namespace std;
 using namespace __gnu_pbds;
 typedef tree<int,null_type,less<int>,rb_tree_tag,tree_order_statistics_node_update> set_t;
+
+#include <ext/pb_ds/assoc_container.hpp>
+using namespace __gnu_pbds;
+typedef cc_hash_table<int,int> umap_t;
 
 int main()
 {
@@ -11,7 +16,7 @@ int main()
 
   // The order of the keys should be: 12, 505.
   assert(*s.find_by_order(0) == 12);
-  assert(*s.find_by_order(3) == 505);
+  assert(s.find_by_order(2) == end(s));
 
   // The order of the keys should be: 12, 505.
   assert(s.order_of_key(12) == 0);
@@ -25,5 +30,6 @@ int main()
 
   // The order of the keys should be: 505.
   assert(s.order_of_key(505) == 0);
-
 }
+
+
