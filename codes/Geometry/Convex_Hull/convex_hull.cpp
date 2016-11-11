2b0443a48aa01d6915dfb72453a42a4d64077f6d
Meteor <bobogei81123@gmail.com>
2016-11-11 20:21:13 +0800

    fix convex hull

--- codes/Geometry/Convex_Hull/convex_hull.cpp
+++ codes/Geometry/Convex_Hull/convex_hull.cpp
@@ -3,12 +3,12 @@ vector<pdd> convex_hull(vector<pdd> pt){
   int top=0;
   vector<pdd> stk(2*pt.size());
   for (int i=0; i<(int)pt.size(); i++){
-    while (top >= 2 && cross(stk[top-2],stk[top-1],pt[i]) <= 0)
+    while (top >= 2 && cross(stk[top-1],pt[i],stk[top-2]) <= 0)
       top--;
     stk[top++] = pt[i];
   }
   for (int i=pt.size()-2, t=top+1; i>=0; i--){
-    while (top >= t && cross(stk[top-2],stk[top-1],pt[i]) <= 0)
+    while (top >= t && cross(stk[top-1],pt[i], stk[top-2]) <= 0)
       top--;
     stk[top++] = pt[i];
   }
